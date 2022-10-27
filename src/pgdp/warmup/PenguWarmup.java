74121fad385d388a00bf91f52598b721c05686a1
Said Tamim Sadat <ge87mit@mytum.de>
2022-10-27 20:53:30 +0200

    test

--- src/pgdp/warmup/PenguWarmup.java
+++ src/pgdp/warmup/PenguWarmup.java
@@ -2,12 +2,35 @@ package pgdp.warmup;
 
 public class PenguWarmup {
 
+	/*	Die Inhalte der main()-Methode beeinflussen nicht die Bewertung dieser Aufgabe
+	 *	(es sei denn natürlich, sie verursachen Compiler-Fehler).
+	 */
+	public static void main(String[] args) {
+
+		// Here is a place for you to play around :)
+
+		penguInfoOut(6);
+		System.out.println(penguEvolution(1337, 5));
+	}
+
 	protected PenguWarmup() {
 		throw new UnsupportedOperationException();
 	}
 
 	public static void penguInfoOut(int penguin) {
 		// TODO
+
+		if (penguin < 0) {
+			System.out.println("Penguin " + penguin + " is not a known penguin!");
+		} else if (penguin % 2 == 0) {
+			System.out.println("Penguin: " + penguin);
+			System.out.println("This penguin is a male.");
+		}
+		else {
+			System.out.println("Penguin: " + penguin);
+			System.out.println("This penguin is a female.");
+		}
+
 	}
 
 	public static int penguEvolution(int penguin, int years) {
@@ -30,15 +53,6 @@ public class PenguWarmup {
 		return 0;
 	}
 
-	/*	Die Inhalte der main()-Methode beeinflussen nicht die Bewertung dieser Aufgabe
-	 *	(es sei denn natürlich, sie verursachen Compiler-Fehler).
-	 */
-	public static void main(String[] args) {
 
-		// Here is a place for you to play around :)
-
-		penguInfoOut(1);
-		System.out.println(penguEvolution(1337, 5));
-	}
 
 }
